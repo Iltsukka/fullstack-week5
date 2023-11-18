87c333973eda6b72a0593519712b9895e0d0231b
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-11-18 13:22:05 +0200

    exercise 5.18

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -1,3 +1,4 @@
+
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
@@ -6,5 +7,27 @@ describe('Blog app', function() {
 
   it('Login form is shown', function() {
     cy.contains('log in to application')
+  })
+})
+
+describe('Login',function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http:localhost:3003/api/users', { 'username': 'kana', 'name':'kanamies', 'password':'kanala123' })
+    cy.visit('http://localhost:5173')
+  })
+  it('succeeds with correct credentials', function() {
+    cy.get('#username').type('kana')
+    cy.get('#password').type('kanala123')
+    cy.get('#login-button').click()
+    cy.contains('blog')
+    cy.contains('kanamies logged in')
+  })
+
+  it('fails with wrong credentials', function() {
+    cy.get('#username').type('kana')
+    cy.get('#password').type('vääräsalasana')
+    cy.get('#login-button').click()
+    cy.contains('Wrong username or password')
   })
 })
